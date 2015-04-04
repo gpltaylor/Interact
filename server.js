501d9e6d85ee0b29b5e5e26dcd7184823c9a8eab
Garry Taylor <giz@gbdesign.net>
2015-04-04 12:59:40 +0100

    Server
    Update Server to serve files from root location.

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+var port = process.env.port || 1337;
+
+// Serve up public/ftp folder
+var serve = serveStatic('.', {'index': ['index.html', 'index.htm']})
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+// Listen
+server.listen(port);
