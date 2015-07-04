7a8e446883582caa0d789f90daf5098df85066dc
Garry Taylor <giz@gbdesign.net>
2015-07-04 12:16:31 +0100

    Include some help page updates to get the ball rolling with this. Bit simple at the moment but will refactor at some point

--- app/Home/routes.js
+++ app/Home/routes.js
@@ -6,5 +6,17 @@ angular.module('RxApp.Home', ['ngRoute'])
     $routeProvider.when('/', {
         templateUrl: 'App/Home/home.html',
         controller: 'MenuCtrl'
+    }).when('/AboutUs', {
+        templateUrl: 'App/Home/aboutus.html',
+        controller: 'MenuCtrl'
+    }).when('/GettingStarted', {
+        templateUrl: 'App/Home/gettingstarted.html',
+        controller: 'MenuCtrl'
+    }).when('/ContactUs', {
+        templateUrl: 'App/Home/contactus.html',
+        controller: 'MenuCtrl'
+    }).when('/Contribute', {
+        templateUrl: 'App/Home/contribute.html',
+        controller: 'MenuCtrl'
     });
 }]);
